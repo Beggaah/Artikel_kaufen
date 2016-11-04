15b14b8edfe13858e6162e2bdc2151227b0bed7b
Johannes Becker <j.becker@faz.de>
2016-11-04 15:52:50 +0100

    Add project files.

---
+++ Artikel_kaufen/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artikel_kaufen
+{
+
+    class Program
+    {
+        double einzelpreis, menge, rabatt, gesamtpreis;
+        Boolean neuerArtikel = true;
+        static void Main(string[] args)
+        {
+            Program aufgabeA = new Program();
+            Program aufgabeB = new Program();
+
+            aufgabeA.Ausgabe();
+        }
+
+        public void Ausgabe()
+        {
+            artikel();
+            rabattF();
+            Console.WriteLine("Rabatt:              " + rabatt);
+            Console.Write("Gesamtpreis:         " + gesamtpreis);
+            Console.ReadKey();
+        }
+
+        public void rabattF()
+        {
+            double oldGesamt = gesamtpreis;
+
+            if (gesamtpreis >= 100 && gesamtpreis < 500)
+            {
+                gesamtpreis = gesamtpreis * 0.95;
+            }
+            else if (gesamtpreis >= 500)
+            {
+                gesamtpreis = gesamtpreis * 0.93;
+            }
+            rabatt = oldGesamt - gesamtpreis;
+        }
+
+
+
+        public void artikel()
+        {
+            while (neuerArtikel == true)
+            {
+                Console.Write("Einzelpreis:         ");
+                einzelpreis = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Menge:               ");
+                menge = Convert.ToDouble(Console.ReadLine());
+                gesamtpreis = gesamtpreis + (menge * einzelpreis);
+
+                Console.Write("weiter Artikel?  ");
+
+                if (Console.ReadLine() == "j")
+                {
+                    neuerArtikel = true;
+                }
+                else
+                    neuerArtikel = false;
+            }
+        }
+    }
+}
